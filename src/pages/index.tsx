ad87055fe5a9045f428e00f54d5194cf704c9b7d
Bruno Mateus <bruno_205@hotmail.com>
2022-10-20 05:55:13 -0300

    Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
+import { styled } from "../styles"
+
+const Button = styled("button", {
+  backgroundColor:'$rocketseat',
+  borderRadius:4,
+  padding:"4px 8px",
+  span:{
+    fontWeight:'bold'
+  },
+  '&:hover':{
+    cursor:'pointer',
+  }
+})
 
 export default function Home() {
-  return <h1>Hello world</h1>
+
+
+
+  return <Button>Sent <span>Now</span></Button>
   
 }
