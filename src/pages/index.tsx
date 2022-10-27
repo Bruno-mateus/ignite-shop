1926b65012631ab17a0892cb5cfd1fe5c960914d
Bruno Mateus <bruno_205@hotmail.com>
2022-10-27 05:23:16 -0300

    buscando produtos do stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,50 @@
+import { HomeContainer, Product } from "../styles/pages/home"
+
+import {stripe} from '../lib/stripe'
+
 import Image from "next/image"
 
-import { HomeContainer, Product } from "../styles/pages/home"
-import tShirt1 from '../t-shirts/1.png'
-import tShirt2 from '../t-shirts/2.png'
-import tShirt3 from '../t-shirts/3.png'
-import { useKeenSlider } from 'keen-slider/react'
 
-import 'keen-slider/keen-slider.min.css'
 import { useState } from "react"
+import { GetServerSideProps } from "next"
 
-export default function Home() {
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+import Stripe from "stripe"
 
- 
+
+
+interface HomeProps {
+  products:{
+  id: string;
+  name:string;
+  imgUrl:string;
+  price:number;
+  }[]
+}
+
+
+export const getServerSideProps:GetServerSideProps = async ()=>{
+  const response = await stripe.products.list({
+    expand:['data.default_price']
+  })
+  const products = response.data.map(product=>{
+    const price = product.default_price as Stripe.Price
+    return{
+      id: product.id,
+      name:product.name,
+      imgUrl:product.images[0],
+      price:price.unit_amount/100
+    }
+  })
+  return {
+    props:{
+      products
+    }
+  }
+} 
+
+export default function Home({products}:HomeProps) {
 
 
 const [sliderRef] = useKeenSlider({
@@ -24,35 +57,23 @@ const [sliderRef] = useKeenSlider({
   return (
     
 <HomeContainer ref={sliderRef} className="keen-slider">
-    <Product className="keen-slider__slide">
-      <Image src={tShirt1} width={520}  height={480} alt=""/>
-      <footer>
-        <strong>Camiseta X</strong>
-        <span>R$ 79,98</span>
-      </footer>
-    </Product>    
-    <Product className="keen-slider__slide">
-      <Image src={tShirt2} width={520}  height={480} alt=""/>
-      <footer>
-        <strong>Camiseta X</strong>
-        <span>R$ 79,98</span>
-      </footer>
-    </Product>
-    <Product className="keen-slider__slide">
-      <Image src={tShirt3} width={520}  height={480} alt=""/>
-      <footer>
-        <strong>Camiseta X</strong>
-        <span>R$ 79,98</span>
-      </footer>
-    </Product>
-    <Product className="keen-slider__slide">
-      <Image src={tShirt3} width={520}  height={480} alt=""/>
-      <footer>
-        <strong>Camiseta X</strong>
-        <span>R$ 79,98</span>
-      </footer>
-    </Product>
+    {
+      products.map(product =>{
+        return(
+        <Product className="keen-slider__slide" key={product.id}>
+          <Image  src={product.imgUrl} width={520} height={480} alt=""/>
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{
+            new Intl.NumberFormat('pt-br',{style:'currency', currency:'BRL'}).format(product.price)
+      }</span>
+          </footer>
+        </Product>
+      )})
+    }
 </HomeContainer>
   )
   
+  
 }
+
